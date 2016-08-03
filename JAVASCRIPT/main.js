b52171fcd8fc6f7850e87a671bd08a6d87caef80
Rich Ranallo <zincoxide@gmail.com>
2016-08-03 18:16:59 -0500

    added phone and correctly formatted alderman titles

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -1,11 +1,17 @@
-var wardMatch = function(lng, lat, addr) {
-	var results = leafletPip.pointInLayer([lng, lat], districts);
+var wardMatch = function(location) {
+	var results = leafletPip.pointInLayer([location.lng, location.lat], districts);
 	if (results.length > 0) {
 		return {
 			ward: results[0].feature.properties,
-			normAddress: addr
+			normAddress: location.formatted
 		};
 	} else {
 		return false;
 	}
 }
+
+console.log(wardMatch({
+  "lat": 41.962087,
+  "lng": -87.670701,
+  "formatted": "4417 N Paulina St, Chicago, IL 60640"
+}));

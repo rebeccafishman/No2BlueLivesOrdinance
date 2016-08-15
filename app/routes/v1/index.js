9ab12b8bc7fd8d7b7144a08ec15096508e6065ac
Rich Ranallo <zincoxide@gmail.com>
2016-08-15 12:11:06 -0500

    subbing Chicago in for every city/state

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -8,34 +8,40 @@ var parser = require('parse-address');
 (function() {
     module.exports = {
        address: function ( req, res ) {
-		var address = parser.parseLocation(req.query.a);
-		address.city = 'Chicago';
-		address.state = 'IL';
+			var address = parser.parseLocation(req.query.a);
 
-		var geocodioAddress = address.number;
-		if (address.prefix) geocodioAddress += ' ' + address.prefix;
-		if (address.street) geocodioAddress += ' ' + address.street;
-		if (address.type) geocodioAddress += ' ' + address.type;
-		if (address.city) geocodioAddress += ' ' + address.city;
-		if (address.state) geocodioAddress += ' ' + address.state;
-		if (address.zip) geocodioAddress += ' ' + address.zip;
+			if (address.number) {
+				var geocodioAddress = address.number;
+				address.city = 'Chicago';
+				address.state = 'IL';
+				if (address.prefix) geocodioAddress += ' ' + address.prefix;
+				if (address.street) geocodioAddress += ' ' + address.street;
+				if (address.type) geocodioAddress += ' ' + address.type;
+				if (address.city) geocodioAddress += ' ' + 'Chicago';
+				if (address.state) geocodioAddress += ' ' + 'IL';
+				if (address.zip) geocodioAddress += ' ' + address.zip;
+				
+				geocodio.get('geocode', {q: geocodioAddress}, function(err, response){
+				    if (err) {
+				    	res.sendStatus(400, err);
+				    } else {
+					    var result = JSON.parse(response).results[0];
 
-	  console.log(geocodioAddress);
+				       	res.send({
+				       		lat:result.location.lat,
+				       		lng:result.location.lng,
+				       		formatted:result.formatted_address
+				       	});
+				    }
+				});
 
-		geocodio.get('geocode', {q: geocodioAddress}, function(err, response){
-		    if (err) {
-		    	console.log(err);
-		    	res.sendStatus(400, err);
-		    } else {
-			    var result = JSON.parse(response).results[0];
-			    console.log(result);
-		       	res.send({
-		       		lat:result.location.lat,
-		       		lng:result.location.lng,
-		       		formatted:result.formatted_address
-		       	});
-		    }
-		});
+			} else {
+				res.sendStatus(400, 'need street address');
+			}
+
+
+
+
        }
     }
 }).call(this);

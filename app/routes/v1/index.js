32e6c51c376a06d0d5f8b83a20a431165c5e06c2
Rich Ranallo <zincoxide@gmail.com>
2016-08-15 10:42:02 -0500

    added procfile with web dev script

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -3,14 +3,29 @@ var CONFIG = require('config').BASE;
 
 var geocodio = new Geocodio(CONFIG.geocodio);
 
+var parser = require('parse-address');
+
 (function() {
     module.exports = {
        address: function ( req, res ) {
-		var address = req.query.a;
+		var address = parser.parseLocation(req.query.a);
+		address.city = 'Chicago';
+		address.state = 'IL';
 
-		geocodio.get('geocode', {q: address}, function(err, response){
+		var geocodioAddress = address.number;
+		if (address.prefix) geocodioAddress += ' ' + address.prefix;
+		if (address.street) geocodioAddress += ' ' + address.street;
+		if (address.type) geocodioAddress += ' ' + address.type;
+		if (address.city) geocodioAddress += ' ' + address.city;
+		if (address.state) geocodioAddress += ' ' + address.state;
+		if (address.zip) geocodioAddress += ' ' + address.zip;
+
+	  console.log(geocodioAddress);
+
+		geocodio.get('geocode', {q: geocodioAddress}, function(err, response){
 		    if (err) {
-		    	res.sendStatus(400);
+		    	console.log(err);
+		    	res.sendStatus(400, err);
 		    } else {
 			    var result = JSON.parse(response).results[0];
 			    console.log(result);

8001a9935c3563dd7a8cbc8d6ac3a54ac87f1da9
Rich Ranallo <zincoxide@gmail.com>
2016-08-16 20:58:40 -0500

    protocol matching for heroku

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -46,6 +46,6 @@ var addressSearch = function() {
 	  }
 	};
 
-	xhttp.open("GET", "http://bluestlie.herokuapp.com/v1/address?a=" + encodeURIComponent(address), true);
+	xhttp.open("GET", "//bluestlie.herokuapp.com/v1/address?a=" + encodeURIComponent(address), true);
 	xhttp.send();
 };

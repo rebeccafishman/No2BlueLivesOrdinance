36d63f7ef134d0a0e22fd225a23e582690e4d782
7Red7Chocobo7 <hitch723@gmail.com>
2016-08-23 20:34:20 -0500

    Update main.js

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -12,7 +12,7 @@ var wardMatch = function(location) {
 //Code with Class [0] and [1]
 var addressSearch = function() {
 	event.preventDefault();
-	var address = document.getElementsByClassName[0]('address').elements['address-input'].value;
+	var address = document.getElementById[0]('address').elements['address-input'].value;
 
 	var xhttp;
 	if (window.XMLHttpRequest) {
@@ -24,37 +24,37 @@ var addressSearch = function() {
 
 	xhttp.onreadystatechange = function() {
 	  if (xhttp.readyState < 4) {
-	  	document.getElementsByClassName[0]('address-input').disabled=true;
-	  	document.getElementsByClassName[0]('address-submit').disabled=true;
-	  	document.getElementsByClassName[1]('address-input').disabled=true;
-	  	document.getElementsByClassName[1]('address-submit').disabled=true;
+	  	document.getElementsByClassName('address-input')[0].disabled=true;
+	  	document.getElementsByClassName('address-submit')[0].disabled=true;
+	  	document.getElementsByClassName('address-input')[1].disabled=true;
+	  	document.getElementsByClassName('address-submit')[1].disabled=true;
 	  } else if (xhttp.readyState == 4) {
-	  	document.getElementsByClassName[0]('address-input').disabled=false;
-	  	document.getElementsByClassName[0]('address-submit').disabled=false;
-	  	document.getElementsByClassName[1]('address-input').disabled=false;
-	  	document.getElementsByClassName[1]('address-submit').disabled=false;
+	  	document.getElementsByClassName('address-input')[0].disabled=false;
+	  	document.getElementsByClassName('address-submit')[0].disabled=false;
+	  	document.getElementsByClassName('address-input')[1].disabled=false;
+	  	document.getElementsByClassName('address-submit')[1].disabled=false;
 	  }
 	  if (xhttp.readyState == 4 && xhttp.status == 200) {
 	    console.log(wardMatch(JSON.parse(xhttp.responseText)));
 	    var ward = wardMatch(JSON.parse(xhttp.responseText));
-	    document.getElementsByClassName[0]('formatted-address').innerHTML = ward.normAddress;
-	    document.getElementsByClassName[0]('alderman-name').innerHTML = ward.ward.council_member;
-	    document.getElementsByClassName[0]('alderman-phone').innerHTML = ward.ward.phone;
-	    document.getElementsByClassName[0]('alderman-ward').innerHTML = ward.ward.ward;
-	    document.getElementsByClassName[0]('alderman-profile').setAttribute('class', '');
-	    document.getElementsByClassName[0]('alderman-err').setAttribute('class', 'hidden');
-	    document.getElementsByClassName[1]('formatted-address').innerHTML = ward.normAddress;
-	    document.getElementsByClassName[1]('alderman-name').innerHTML = ward.ward.council_member;
-	    document.getElementsByClassName[1]('alderman-phone').innerHTML = ward.ward.phone;
-	    document.getElementsByClassName[1]('alderman-ward').innerHTML = ward.ward.ward;
-	    document.getElementsByClassName[1]('alderman-profile').setAttribute('class', '');
-	    document.getElementsByClassName[1]('alderman-err').setAttribute('class', 'hidden');
+	    document.getElementsByClassName('formatted-address')[0].innerHTML = ward.normAddress;
+	    document.getElementsByClassName('alderman-name')[0].innerHTML = ward.ward.council_member;
+	    document.getElementsByClassName('alderman-phone')[0].innerHTML = ward.ward.phone;
+	    document.getElementsByClassName('alderman-ward')[0].innerHTML = ward.ward.ward;
+	    document.getElementsByClassName('alderman-profile')[0].setAttribute('class', '');
+	    document.getElementsByClassName('alderman-err')[0].setAttribute('class', 'hidden');
+	    document.getElementsByClassName('formatted-address')[1].innerHTML = ward.normAddress;
+	    document.getElementsByClassName('alderman-name')[1].innerHTML = ward.ward.council_member;
+	    document.getElementsByClassName('alderman-phone')[1].innerHTML = ward.ward.phone;
+	    document.getElementsByClassName('alderman-ward')[1].innerHTML = ward.ward.ward;
+	    document.getElementsByClassName('alderman-profile')[1].setAttribute('class', '');
+	    document.getElementsByClassName('alderman-err')[1].setAttribute('class', 'hidden');
 	  } else if (xhttp.readyState == 4 && xhttp.status >= 300){
 
-	  	document.getElementsByClassName[0]('alderman-profile').setAttribute('class', 'hidden');
-	  	document.getElementsByClassName[0]('alderman-err').setAttribute('class', '');
-	  	document.getElementsByClassName[1]('alderman-profile').setAttribute('class', 'hidden');
-	  	document.getElementsByClassName[1]('alderman-err').setAttribute('class', '');
+	  	document.getElementsByClassName('alderman-profile')[0].setAttribute('class', 'hidden');
+	  	document.getElementsByClassName('alderman-err')[0].setAttribute('class', '');
+	  	document.getElementsByClassName('alderman-profile')[1].setAttribute('class', 'hidden');
+	  	document.getElementsByClassName('alderman-err')[1].setAttribute('class', '');
 	  }
 	};
 
@@ -62,44 +62,3 @@ var addressSearch = function() {
 	xhttp.send();
 };
 
-/* Code with Id
-var addressSearch = function() {
-	event.preventDefault();
-	var address = document.getElementById('address').elements['address-input'].value;
-
-	var xhttp;
-	if (window.XMLHttpRequest) {
-		    xhttp = new XMLHttpRequest();
-	    } else {
-		    // code for IE6, IE5
-		    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-
-	xhttp.onreadystatechange = function() {
-	  if (xhttp.readyState < 4) {
-	  	document.getElementById('address-input').disabled=true;
-	  	document.getElementById('address-submit').disabled=true;
-	  } else if (xhttp.readyState == 4) {
-	  	document.getElementById('address-input').disabled=false;
-	  	document.getElementById('address-submit').disabled=false;
-	  }
-	  if (xhttp.readyState == 4 && xhttp.status == 200) {
-	    console.log(wardMatch(JSON.parse(xhttp.responseText)));
-	    var ward = wardMatch(JSON.parse(xhttp.responseText));
-	    document.getElementById('formatted-address').innerHTML = ward.normAddress;
-	    document.getElementById('alderman-name').innerHTML = ward.ward.council_member;
-	    document.getElementById('alderman-phone').innerHTML = ward.ward.phone;
-	    document.getElementById('alderman-ward').innerHTML = ward.ward.ward;
-	    document.getElementById('alderman-profile').setAttribute('class', '');
-	    document.getElementById('alderman-err').setAttribute('class', 'hidden');
-	  } else if (xhttp.readyState == 4 && xhttp.status >= 300){
-
-	  	document.getElementById('alderman-profile').setAttribute('class', 'hidden');
-	  	document.getElementById('alderman-err').setAttribute('class', '');
-	  }
-	};
-
-	xhttp.open("GET", "https://bluestlie.herokuapp.com/v1/address?a=" + encodeURIComponent(address), true);
-	xhttp.send();
-};
-/*
